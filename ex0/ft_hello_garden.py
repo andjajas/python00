29700ef37bc427aff1fb646c387ef69c2a20bc07
Andre Djajasaputra <andjajas@f1r6s6.codam.nl>
2026-06-08 19:47:29 +0200

    ex0, ex1 done

--- ex0/ft_hello_garden.py
+++ ex0/ft_hello_garden.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
-def ft_hello_garden():
-	print("Hello, Garden Community!")
+def ft_hello_garden() -> None:
+    print("Hello, Garden Community!")
---
+++ ex1/ft_garden_name.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+def ft_garden_name() -> None:
+    var = input("Enter garden name: ")
+    print(f"Garden: {var}")
+    print("Growing well!")
