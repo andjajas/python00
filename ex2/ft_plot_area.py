ad6a32af9d2891b921b5e844887c4b795a7df31f
Andre Djajasaputra <andjajas@f1r6s6.codam.nl>
2026-06-08 20:36:39 +0200

    ex2 done

---
+++ ex2/ft_plot_area.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def ft_plot_area() -> None:
+    length = input("Enter length: ")
+    width = input("Enter width: ")
+    plot_area = int(length) * int(width)
+    print(f"Plot area: {plot_area}")
